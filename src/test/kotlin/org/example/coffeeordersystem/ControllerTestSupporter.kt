cb8b170d1de7bcfc5206b28d2edd16669b3488e3
ch.lee <ch.lee@promtech.co.kr>
2025-02-18 02:34:48 +0900

    <Refactor> : security 적용으로 인한 테스트 코드 리팩토링

--- src/test/kotlin/org/example/coffeeordersystem/ControllerTestSupporter.kt
+++ src/test/kotlin/org/example/coffeeordersystem/ControllerTestSupporter.kt
@@ -1,10 +1,16 @@
 package org.example.coffeeordersystem
 
+import org.example.coffeeordersystem.config.security.JwtAuthenticationFilter
+import org.example.coffeeordersystem.config.security.JwtTokenProvider
+import org.example.coffeeordersystem.config.security.SecurityConfig
 import org.example.coffeeordersystem.service.MenuService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Import
 import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.test.web.servlet.MockMvc
 
+
+@Import(SecurityConfig::class, JwtTokenProvider::class, JwtAuthenticationFilter::class)
 abstract class ControllerTestSupporter {
 
     @Autowired
