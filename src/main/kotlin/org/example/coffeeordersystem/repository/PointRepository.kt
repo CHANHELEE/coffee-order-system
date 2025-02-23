9c3a0fc726c5cfa870ed9f7bcd64821fde4b051c
ch.lee <ch.lee@promtech.co.kr>
2025-02-23 20:26:03 +0900

    <Feat> : 포인트 업데이트시 행 단위 쓰기락 추가

--- src/main/kotlin/org/example/coffeeordersystem/repository/PointRepository.kt
+++ src/main/kotlin/org/example/coffeeordersystem/repository/PointRepository.kt
@@ -1,11 +1,14 @@
 package org.example.coffeeordersystem.repository
 
+import jakarta.persistence.LockModeType
 import org.example.coffeeordersystem.model.entity.Point
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.stereotype.Repository
 
 @Repository
 interface PointRepository : JpaRepository<Point, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     fun findByAccountId(accountId: Long) : Point
 }
